c137f2abb3e1dc9e6deccb29385c5da2751d8982
mayhendrap <mayhendra.p10@gmail.com>
2021-03-17 17:21:59 +0700

    belajar interface

---
+++ interface/main.go
@@ -0,0 +1,26 @@
+package main
+
+type Luas interface {
+	HitungLuas() int
+}
+
+type Persegi struct {
+	Sisi int
+}
+
+type PersegiPanjang struct {
+	Panjang int
+	Lebar   int
+}
+
+func (persegi Persegi) HitungLuas() int {
+	return persegi.Sisi * persegi.Sisi
+}
+
+func (persegiPanjang PersegiPanjang) HitungLuas() int {
+	return persegiPanjang.Panjang * persegiPanjang.Lebar
+}
+
+func main() {
+
+}
